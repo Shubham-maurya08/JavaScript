dfc36643f54f43fca270e7be759f8fde8119038c
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2023-12-19 12:59:36 +0530

    Add files via upload

---
+++ Day 5 (Comparison Operator)/index.js
@@ -0,0 +1,47 @@
+// Comparison Operator
+
+// let a = 5;      //Comparison between two values
+// let b = 6;
+// console.log(a==b); //false 
+
+// let c = 5;      //Comparison between two values
+// let d = '5';
+// console.log(c === d); //false value is but datatype is different thats why we got false 
+
+// let e = 10;
+// let f = '10';
+// console.log(e==f); //true
+
+// let sam = 10;
+// let shubham = '10';
+// console.log(sam !== shubham); //true
+
+// let sam = 10;
+// let shubham = 10;
+// console.log(sam !== shubham); //false
+
+// let sam = 10;
+// let shubham = 5;
+// console.log(sam > shubham); //true
+
+// let sam = 10;
+// let shubham = 5;
+// console.log(sam >= shubham); //true
+
+// let sam = 10;
+// let shubham = 5;
+// console.log(sam += shubham); //15
+
+// let sam = 5;
+// let shubham = 10;
+// console.log(sam -= shubham); //-5
+
+// let sam = 100;
+// let shubham = 20;
+// console.log(sam % shubham); //0
+
+let sam = 100;
+let shubham = 20;
+console.log(sam /= shubham); //5
+
+
