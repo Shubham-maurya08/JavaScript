06163cf76f28dcea397776ece9bd45231e26bdca
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2023-12-19 12:58:30 +0530

    Add files via upload

---
+++ Day 2 (num,bool,string)/index.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+var message;
+
+message = "This is website";
+console.log(message);
+
+var x = 5; // Number Data Type
+var y = "samworld";// String Data Type
+var z = "try";
+console.log(x,y,z);
+
+var a = 5;
+var b = 10;
+console.log(a==b); //Boolean Data Type
