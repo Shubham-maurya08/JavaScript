4c4ea894d984aff1049b6113d9b749fea1d9aae8
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2023-12-20 19:20:46 +0530

    Add files via upload

---
+++ Day 6 (Inc,Dec,Logical Operator,prompt)/index.js
@@ -0,0 +1,39 @@
+//Increment and Decrement Operator
+
+// a++ Post Increment
+// ++a Pre Increment
+// a-- Post Decrement
+// --a Pre Decrement
+
+// let a = 5; //post
+// console.log(a++, a,a,a);
+
+// let a = 5;
+// console.log(++a,a,a,a); //pre
+
+// let a = 5;
+// console.log(a--, a,a,a); //post
+
+// let a = 5;
+// console.log(--a, a,a,a); //pre
+
+
+//Logical Operator &&, ||, !
+
+console.log(5 > 10 && 2 < 5); //false
+console.log(5 > 10 || 2 < 3); //true
+console.log(!true);
+console.log(!false);
+
+//Question-1
+// let A = 2;
+// let B = 5;
+// console.log(A+B);
+
+//Question-2
+// let A = 5;
+// let B = 3;
+// console.log(A<10 && A>B);
+
+let Name = prompt("Enter your Name");// In prompt we get all values, like number, string, boolean
+console.log(Name);
