3fd5aaaa2432e4b0e5a5bef2864c79a0af3eaecb
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2023-12-21 17:07:48 +0530

    Add files via upload

---
+++ Day 7 (Conditional and Ternary Operator)/index.js
@@ -0,0 +1,12 @@
+//Conditional and Ternary Operator
+
+//Condition ? val1 : val2
+
+let a = 5;
+let b = '6';
+let c = 3;
+let d = 7;
+let message = "This condition is false";
+console.log(a === b ? 'Answer-1' : 'Answer-2', c === d ? "Green" : message); 
+
+//If condition will be true than 1st answer will be print else 2nd answer will be print.
