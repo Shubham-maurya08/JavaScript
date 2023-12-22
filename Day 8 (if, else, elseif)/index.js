415eda1877fbaaee0ad29e0d040205d57af99c02
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2023-12-22 12:10:00 +0530

    Add files via upload

---
+++ Day 8 (if, else, elseif)/index.js
@@ -0,0 +1,53 @@
+//Dicision Making
+
+// if/else
+// let x = 5;
+// if(x > 10){
+//     console.log('x is greater than 10');
+// }else{
+//     console.log('x is less than or equal 10');
+// }
+
+
+//Q:
+
+let P;
+let p;
+let Z;
+let z;
+let E;
+let e;
+let D;
+let d;
+let output;
+let input = prompt('enter a character');
+if (input === 'p' || input === 'P') {
+    output = 'PrepBytes'
+    console.log(output);
+} else if (input === 'z' || input === 'Z') {
+    output = 'Zenith'
+    console.log(output);
+} else if (input === 'e' || input === 'E') {
+    output = 'Expert Coder'
+    console.log(output);
+} else if (input === 'd' || input === 'D') {
+    output = 'Data Structure'
+    console.log(output);
+} else {
+    console.log('Invalid input');
+}
+
+
+// let a = parseInt(prompt('Enter the value of a',""));
+// let b = parseInt(prompt('Enter the value of a',""));
+// let c = parseInt(prompt('Enter the value of a',""));
+// if(a>b && a>c){
+//     console.log("Max number is" + a);
+// }else if(b>a && b>c){
+//     console.log("Max number is" + b);
+// }else if(c>a && c>b){
+//     console.log("Max number is" + c);
+// }else{
+//     console.log("Try Again");
+// }
+
