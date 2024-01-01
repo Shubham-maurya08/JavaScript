f4bcf7268a17df5cb8b8aae4296875c35d9d0d83
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2024-01-01 18:20:26 +0530

    Add files via upload

---
+++ Day 11 (Hoisting & for loop)/index.js
@@ -0,0 +1,24 @@
+//Hoisting
+
+//Only var keyword access in hoisting at JS, cause var is global scope.
+//not let and const.
+
+// a = 10;            
+// console.log(a); it will work.
+// var a;
+
+// let a;
+// console.log(a);  //Undefined
+// a = 10;
+
+
+//for Loop
+
+// const n = 5;
+// for(let i = 1; i <= n; i++){
+//     console.log("I am a Developer");
+//     console.log(i);
+// }
+//Output = 5 times print "I am a Developer".
+
+
