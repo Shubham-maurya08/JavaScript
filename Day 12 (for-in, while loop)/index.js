a41210e92df2681e4ac03ee35cfbc1e1ea8fb47b
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2024-01-02 22:35:59 +0530

    Add files via upload

---
+++ Day 12 (for-in, while loop)/index.js
@@ -0,0 +1,34 @@
+//for-in loop
+
+// const student = {
+//     Name: "Shubham",
+//     class: "JS",
+//     age: 25
+// }
+// for(let key in student){
+//     console.log(`${key} = ${student[key]}`);
+// }
+
+// const student = {
+//     Name: "Shubham",
+//     class: "JS",
+//     age: 25
+// }
+// console.log(student);
+
+//While loop
+// let i = 0;
+// while(i < 10){
+//     i++;
+//    // //console.log("The number is"+ i);
+//     text = "The number is" + i;
+//     i++;
+//     console.log(text);    
+// }
+
+let i = 1;
+while(i < 5){
+    i++;
+    out = "Number is" +" - "+ i;
+    console.log(out);
+}
