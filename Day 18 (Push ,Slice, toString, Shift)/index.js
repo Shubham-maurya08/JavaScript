6638d0683b0333ec3a6c25db1d48362de23630e3
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2024-01-18 11:39:05 +0530

    Add files via upload

---
+++ Day 18 (Push ,Slice, toString, Shift)/index.js
@@ -0,0 +1,32 @@
+// let arr = [34,25,30];
+// console.log(arr);
+
+// let arr1 = ["Name", "Sam", "World"];
+// console.log(arr1);
+
+//There are ample of Methods in Array.
+
+//PUSH
+//Push = its used to add the new value in the last of the array.
+
+// const arr = ["Jarvis", "The"];
+// arr.push("Developer");
+// console.log(arr);
+
+//SLICE
+
+// const arr = ["Jarvish","The","Develeper"]
+// const result = arr.slice(1,2);
+// console.log(result);
+
+//TOSTRING
+
+// const arr = ["Jarvish","The","Develeper"];
+// const res = arr.toString();
+// console.log(res);
+
+//SHIFT
+
+// const arr = ["Jarvish","The","Develeper"];
+// arr.shift();
+// console.log(arr);
