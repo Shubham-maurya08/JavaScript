d8ffa1a259bc38d431308759f5aae40b5dccb289
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2024-01-19 13:17:57 +0530

    Add files via upload

---
+++ Day 19 (map, pop, filter, concat, join, push, shift, unshift, slice, splice, sort, reverse, indexOf, lastIndexOf)/index.js
@@ -0,0 +1,89 @@
+//MAP
+
+// const arr = [1,4,9,16];
+// let res = arr.map(x => x * 2);
+// console.log(res);
+
+// const arrAdd = [2,4,6,8];
+// let result = arrAdd.map(x => x + 2);
+// console.log(result); 
+
+//POP
+//It removes the last array of element.
+// const arr = ["Jarvish", "The", "Developer"]; 
+// arr.pop();
+// console.log(arr);
+
+//FILTER
+
+// const arr = ["Jarvish", "The", "Developer"];
+// let res = arr.filter(word => word.length > 3);
+// console.log(res);
+
+
+//CONCAT
+// let arr1 = [1,2,3]
+// let arr2 = [4,5,6]
+// let res = arr1.concat(arr2);
+// console.log(res);
+
+//JOIN
+// let newarr = ["Jarvish", "The", "Developer"];
+// let result = newarr.join("-");
+// console.log(result);
+
+
+//PUSH
+// let arr = [2,3,4];
+// arr.push(7);
+// console.log(arr);
+
+//POP
+// let arr = [2,3,4];
+// arr.pop();
+// console.log(arr);
+
+//SHIFT
+//remove first element
+// let arr = [2,3,4];
+// arr.shift();
+// console.log(arr);
+
+//UNSHIFT
+
+// let arr = [4,5,6];
+// arr.unshift(1);
+// arr.unshift(2);
+// arr.unshift(3);
+// console.log(arr);
+
+//SLICE
+// let arr = [1,2,3,4,5,6,3];
+// let res = arr.slice(1,3);
+// console.log(res);
+
+//SPLICE
+//let arr = [1,3,'a','b','c','d'];
+//arr.splice(2,2,"Hello");// First value is index value and 2nd value is replace and 3rd  value is adding new value.
+// console.log(arr);
+
+//REVERSE
+// let arr = [1,2,3,4,5,6,3];
+// let res = arr.reverse();
+// console.log(res);
+
+//INDEXOF
+// let arr = ["1","2","3","4","5","6","7"];
+// let res = arr.indexOf("3");
+// console.log(res);
+
+
+//SORT
+// let arr = [4,2,1,4,6,9,3];
+// let res = arr.sort();
+// console.log(res);
+
+//LASTINDEXOF
+// let arr = ["1","2","3","4","5","3","7"];
+// let res = arr.lastIndexOf("3");
+// console.log(res);
