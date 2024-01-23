dcbc062c51b5346bafd6648a4fb2b0b23f58ebb9
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2024-01-23 22:40:57 +0530

    Add files via upload

---
+++ Day 20 (Questions)/index.js
@@ -0,0 +1,34 @@
+//Question:1
+// function check(arr){
+//     if(Array.isArray(arr)){
+//       return true;
+//     }else{
+//       return false;
+//     }
+//   }
+//   console.log(check([1,2,3,4,5]));
+
+//question:2
+// let arr = [1,2,3,4,5,6,"Apple", "Ball", "cat"];
+// console.log(arr);
+
+//Solution:3
+// const array1 = [1,0,2,3,4,0];
+// const array2 = [3,5,6,7,8,13];
+// let sum = array1.map((x, i) => {      //inside function - element/endex/array
+//     return x + array2[i];
+// });
+// console.log(sum);
+
+//Question:4
+let myColor = ["Red", "Green", "White", "Black"];
+let res = myColor.join("+");
+console.log(res);
+
+//Question:5
+// let array = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
+// let res = array.sort((a,b) => a-b);
+// console.log(res);
+
+
+
