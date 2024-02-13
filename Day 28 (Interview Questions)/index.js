31ba016e69929a7facfb1be6b503012f057c5a2e
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2024-02-13 21:03:38 +0530

    Add files via upload

---
+++ Day 28 (Interview Questions)/index.js
@@ -0,0 +1,70 @@
+//Q:1
+// function area(length){
+//     function rec (breadth){
+//         return length*breadth;
+//     }
+//     rec();
+// }
+// console.log(); incomplete
+
+//Q:2
+
+
+//Q:3
+// function newWord(){
+//     document.getElementById("new").innerText = "MERN STACK";
+// }
+
+//Q:4
+// function first(){
+//     const fst = document.getElementsByTagName("p");
+//     console.log(fst[0].innerText);
+// }
+// first();
+
+//Q:5
+function showTime(){
+//     var date = new Date();
+//     var hrs = date.getHours();
+//     var min = date.getMinutes();
+//     var sec = date.getSeconds();
+
+//     var time = hrs + ":" + min + ":" + sec + "";
+//     document.getElementById("MyClockDisplay").innerText = time;
+
+//     document.getElementById("MyClockDisplay").textContent = time;
+
+//     setTimeout(showTime,1000);
+// }
+// showTime();
+
+//Q:6
+// function newChange(){
+//     document.getElementById("name").innerText = "Welcome to Elevation academy";
+// }
+
+//Q:7
+// function hideText(){
+//     document.getElementById("name").innerText = "";
+// }
+
+//Q:8
+// const array=[1,0,0,1,0,1,0,1,1,0];
+// const res = array.filter(x =>x === 0);
+// console.log(res.length);
+
+//Q:9
+// const array1=[1,5,6,8,2,7,9,4,3,1];
+// let evennum = 0;
+// let oddnum = 0;
+
+// for(let i=0; i<array1.length;i++){
+//     if(array1[i] % 2 === 0){
+//         evennum++;
+//     }else{
+//         oddnum++;
+//     }
+// }
+// console.log(evennum, oddnum);
+
+//Q:10 
