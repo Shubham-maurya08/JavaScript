8e6fccb2231fdfaced031340f290a665f8426d20
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2024-02-14 22:50:13 +0530

    Add files via upload

---
+++ Day 32 (Change image )/index.js
@@ -0,0 +1,15 @@
+// function change(){
+//     const image = document.getElementsByTagName("img");
+//     image[0].src = "./what.jpeg";
+//     if (image[0].src == ("img")) {
+//         image.src = "/what.jpeg";
+//       } else {
+//         image.src = ("img");
+//       }
+//     // image[0].style.backgroundImage.url("./what.jpeg");
+// }
+
+
+function change(){
+    document.getElementById("change_img").style.backgroundImage = "url(./what.jpeg)";
+}
